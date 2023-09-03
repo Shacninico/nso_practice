8d1f5115b5e76df5f4df58386609f0c5eabeed0b
Shacninico <nikkoofo.ob@gmail.com>
2023-09-03 20:33:13 +0800

    Laravel 10 Full Course | #14

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
@@ -11,4 +12,14 @@ class AdminController extends Controller
         return view('admin.index');
 
     }//end Method
+
+    public function adminLogout(Request $request){
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    } //end Method Admin Logout
 }
--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,7 @@ require __DIR__ . '/auth.php';
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'adminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'adminLogout'])->name('admin.logout');
 }); //End Group Admin Middleware
 
 Route::middleware(['auth', 'role:agent'])->group(function () {
